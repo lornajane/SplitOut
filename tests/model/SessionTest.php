d17e4f27ae63f9eff063774f7a791529a5c3e2dc
Arne Blankerts <Arne@Blankerts.de>
2011-02-21 12:07:54 +0000

    Ugly demo for the unnamed ;)

--- tests/model/SessionTest.php
+++ tests/model/SessionTest.php
@@ -6,11 +6,30 @@ class SessionTest extends \PHPUnit_Framework_TestCase {
 
    public function testAddingPresentersToASessionWorks() {
       $session = new Session();
-      $user = new User();
+      $userA = new User();
+      $userB = new User();
       
-      $session->addPresenter($user);
+      $session->addPresenter($userA);
+      $session->addPresenter($userB);
       
-      $this->assertContains($user, $session->getPresenters());
+      $this->assertContains($userA, $session->getPresenters());
+      $this->assertContains($userB, $session->getPresenters());
+      
+      return array($session, $userA, $userB);
+   }
+   
+   /**
+    *  THIS IS UGLY LIKE HELL!
+    *
+    * @depends testAddingPresentersToASessionWorks
+    */
+   public function testRemovingPresenterFromASessionWorks(array $bullshit) {
+      list($session, $userA, $userB) = $bullshit;
+   
+      $session->removePresenter($userA);
+      
+      $this->assertContains($userB, $session->getPresenters());
+      $this->assertNotContains($userA, $session->getPresenters());
    }
 
 }
