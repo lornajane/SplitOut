3c4e7c686997fdf61d7f037803e428750362539b
Arne Blankerts <Arne@Blankerts.de>
2011-02-21 10:58:29 +0000

    First idea for adding and removing presenters

--- src/model/Session.php
+++ src/model/Session.php
@@ -3,5 +3,12 @@
 namespace SplitOut\Model;
 
 class Session {
+
+   protected $presenters = array();
+   
+   public function addPresenter(User $presenter) {
+      $this->presenters[] = $presenter;
+   }
+
 }
 
